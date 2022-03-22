34d0081e87ed8310fd0e6ded8e8af4c2ba6fab86
katri <katrin.tramberger@gmail.com>
2022-03-22 14:36:32 +0100

    overview list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,30 @@ import { NavigationComponent } from './navigation/navigation.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { AppRoutingModule } from './app-routing.module';
+import { OverviewListComponent } from './views/overview/overview-list/overview-list.component';
+import {MatListModule} from "@angular/material/list";
+import { OverviewComponent } from './views/overview/overview.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    OverviewListComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    AppRoutingModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
